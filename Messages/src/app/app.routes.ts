5fd24308fa597a86ffe5da21b4dc4a5cc9ef3b2b
Peter Jorgensen <prjorgen@gmail.com>
2025-07-04 18:33:09 -0600

    Seems like I was messing around with login?

--- Messages/src/app/app.routes.ts
+++ Messages/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [  
+    { path: '', component: HomeComponent },
+    //{ path: 'product', component: ProductsComponent, canActivate: [AuthGuard] },
+    { path: 'login', component: LoginComponent }
+];
